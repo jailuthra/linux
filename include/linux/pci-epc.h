1fb01f1bfa53f1656139aea725e536826fb540ef
Kishon Vijay Abraham I <kishon@ti.com>
2018-10-01 14:36:57 +0530

    PCI: endpoint: Assign function number of each PF in EPC core
    The endpoint core relies on the drivers that invoke the API to bind
    a controller driver with a function driver to allocate and assign a
    function number to each physical function. Ideally the function
    numbers should be assinged by EPC core for each of the physical
    function that is bound to the EPC.
    
    Assign function number of each PF in EPC core and remove function
    number allocation in endpoint configfs.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Tested-by: Jean-Jacques Hiblot <jjhiblot@ti.com>
    Signed-off-by: Roger Quadros <rogerq@ti.com>

--- include/linux/pci-epc.h
+++ include/linux/pci-epc.h
@@ -94,6 +94,7 @@ struct pci_epc_mem {
  * @max_functions: max number of functions that can be configured in this EPC
  * @group: configfs group representing the PCI EPC device
  * @lock: mutex to protect pci_epc ops
+ * @function_num_map: bitmap to manage physical function number
  * @notifier: used to notify EPF of any EPC events (like linkup)
  */
 struct pci_epc {
@@ -106,6 +107,7 @@ struct pci_epc {
 	/* mutex to protect against concurrent access of EP controller */
 	struct mutex			lock;
 	unsigned int			features;
+	unsigned long			function_num_map;
 	struct atomic_notifier_head	notifier;
 };
 
