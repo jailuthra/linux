11079bac435e8dc8d7748ae62dbb186621f6d276
Dave Stevenson <dave.stevenson@raspberrypi.com>
2018-10-29 17:57:45 +0000

    media: videobuf2: Allow exporting of a struct dmabuf
    videobuf2 only allowed exporting a dmabuf as a file descriptor,
    but there are instances where having the struct dma_buf is
    useful within the kernel.
    
    Split the current implementation into two, one step which
    exports a struct dma_buf, and the second which converts that
    into an fd.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Tested-by: Paul Elder <paul.elder@ideasonboard.com>
    Signed-off-by: Jai Luthra <jai.luthra@ideasonboard.com>

--- drivers/media/common/videobuf2/videobuf2-core.c
+++ drivers/media/common/videobuf2/videobuf2-core.c
@@ -2419,49 +2419,62 @@ static int __find_plane_by_offset(struct vb2_queue *q, unsigned long offset,
 	return 0;
 }
 
-int vb2_core_expbuf(struct vb2_queue *q, int *fd, struct vb2_buffer *vb,
-		    unsigned int plane, unsigned int flags)
+struct dma_buf *vb2_core_expbuf_dmabuf(struct vb2_queue *q,
+				       struct vb2_buffer *vb,
+				       unsigned int plane, unsigned int flags)
 {
 	struct vb2_plane *vb_plane;
-	int ret;
-	struct dma_buf *dbuf;
+	struct dma_buf *dmabuf;
 
 	if (q->memory != VB2_MEMORY_MMAP) {
 		dprintk(q, 1, "queue is not currently set up for mmap\n");
-		return -EINVAL;
+		return ERR_PTR(-EINVAL);
 	}
 
 	if (!q->mem_ops->get_dmabuf) {
 		dprintk(q, 1, "queue does not support DMA buffer exporting\n");
-		return -EINVAL;
+		return ERR_PTR(-EINVAL);
 	}
 
 	if (flags & ~(O_CLOEXEC | O_ACCMODE)) {
-		dprintk(q, 1, "queue does support only O_CLOEXEC and access mode flags\n");
-		return -EINVAL;
+		dprintk(q, 1, "queue supports only O_CLOEXEC and access mode flags\n");
+		return ERR_PTR(-EINVAL);
 	}
 
 	if (plane >= vb->num_planes) {
 		dprintk(q, 1, "buffer plane out of range\n");
-		return -EINVAL;
+		return ERR_PTR(-EINVAL);
 	}
 
 	if (vb2_fileio_is_active(q)) {
 		dprintk(q, 1, "expbuf: file io in progress\n");
-		return -EBUSY;
+		return ERR_PTR(-EBUSY);
 	}
 
 	vb_plane = &vb->planes[plane];
 
-	dbuf = call_ptr_memop(get_dmabuf,
-			      vb,
-			      vb_plane->mem_priv,
-			      flags & O_ACCMODE);
-	if (IS_ERR_OR_NULL(dbuf)) {
+	dmabuf = call_ptr_memop(get_dmabuf, vb, vb_plane->mem_priv,
+				flags & O_ACCMODE);
+	if (IS_ERR_OR_NULL(dmabuf)) {
 		dprintk(q, 1, "failed to export buffer %d, plane %d\n",
 			vb->index, plane);
-		return -EINVAL;
+		if (!dmabuf)
+			dmabuf = ERR_PTR(-EINVAL);
 	}
+
+	return dmabuf;
+}
+EXPORT_SYMBOL_GPL(vb2_core_expbuf_dmabuf);
+
+int vb2_core_expbuf(struct vb2_queue *q, int *fd, struct vb2_buffer *vb,
+		    unsigned int plane, unsigned int flags)
+{
+	struct dma_buf *dbuf;
+	int ret;
+
+	dbuf = vb2_core_expbuf_dmabuf(q, vb, plane, flags);
+	if (IS_ERR(dbuf))
+		return PTR_ERR(dbuf);
 
 	ret = dma_buf_fd(dbuf, flags & ~O_ACCMODE);
 	if (ret < 0) {
