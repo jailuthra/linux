8fa31cc53250b63582dcb840c4491d75bd59cc13
Peter Ujfalusi <peter.ujfalusi@ti.com>
2020-08-11 13:14:53 +0300

    dmaengine: ti: k3-udma: Fix KSLC alloc_chan_resources handling of enabled channel
    Handle the enabled channel in alloc_chan_resources of pktdma/bcdma in a
    similar way as we do for UDMA.
    Return with a correct return value if the channel can not be stopped and
    use udma_reset_chan() to stop the running channel.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- drivers/dma/ti/k3-udma.c
+++ drivers/dma/ti/k3-udma.c
@@ -2353,9 +2353,10 @@ static int bcdma_alloc_chan_resources(struct dma_chan *chan)
 
 	if (udma_is_chan_running(uc)) {
 		dev_warn(ud->dev, "chan%d: is running!\n", uc->id);
-		udma_stop(uc);
+		udma_reset_chan(uc, false);
 		if (udma_is_chan_running(uc)) {
 			dev_err(ud->dev, "chan%d: won't stop!\n", uc->id);
+			ret = -EBUSY;
 			goto err_res_free;
 		}
 	}
@@ -2522,9 +2523,10 @@ static int pktdma_alloc_chan_resources(struct dma_chan *chan)
 
 	if (udma_is_chan_running(uc)) {
 		dev_warn(ud->dev, "chan%d: is running!\n", uc->id);
-		udma_stop(uc);
+		udma_reset_chan(uc, false);
 		if (udma_is_chan_running(uc)) {
 			dev_err(ud->dev, "chan%d: won't stop!\n", uc->id);
+			ret = -EBUSY;
 			goto err_res_free;
 		}
 	}
