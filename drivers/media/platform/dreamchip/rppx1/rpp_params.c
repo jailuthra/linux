02755b7c3874dfd5d294411a99ad2b39f2331c37
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-03-14 22:59:33 +0100

    media: rppx1: Add framework to support Dreamchip RPPX1 ISP
    Add a framework driver for Dreamchip RPPX1 ISP. The driver aims to
    provide a framework for other V4L2 based drivers to drive the RPPX1
    functionality. The reason for this split is that the RPPX1 IP itself do
    not provide any DMA engines to drive data to/from the device, instead it
    depends on other IP blocks to implement these features.
    
    While the peripherals around the RPPX1 ISP used in different designs and
    by different vendors are different the RPPX1 core itself is the same.
    For this reason the framework solution to be able to split the Dreamchip
    RPPX1 driver from vendors usage of it have been picked in hope to reduce
    duplication of the common parts.
    
    The functions provided by the RPPX1 are similar to what is exposed by
    other ISP drivers already in tree (RkISP1 primarily), but the
    implementation of them are different.
    
    The design is to try and keep the surface of this framework as small as
    possible. The intention of this change is to be able to fill all needs
    of this.
    
      * Two functions to create and destroy a RPPX1 instance, rppx1_create()
        and rppx1_destory(). These are intended to be called in the users
        probe and remove code paths.
    
      * Two functions to start and stop the RPPX1 processing, rppx1_start()
        and rppx1_stop(). These are intended to be called in the users
        stream on and stream off code paths.
    
      * One function to ask the RPPX1 to process parameters buffer prepared
        by user space, rppx1_params(). This is intended to translate the
        parameter buffer to the register writes needed to be preformed on
        the RPPX1. The intention is to call this function when the parameter
        buffer is queued to the V4L2 driver and the result stored by the
        driver until the time it needs to be written to the RPPX1. It's the
        users responsibility to write it either using MMIO or other means.
    
      * One function to fill in a statistic buffer based on the current
        status of the RPPX1, rppx1_stats_fill_isr(). The intention is that
        the user call's this in its interrupt handler when it knows the
        RPPX1 is done processing a frame.
    
      * One function to ack and retrieve the interrupts generated by the
        RPPX1, rppx1_interrupt(). The intention is to call this function
        when the users interrupt handler detects the RPPX1 have raised and
        interrupt. There is no need for the user to understand, or act, on
        the actual RPPX1 interrupt, but it can if it wants too.
    
    The initial support in the framework is limited and do not implement any
    ISP processing algorithms other then configuring the RPPX1 to process
    any Bayer (8-, 10, or 12-bit) image and produce either a RGB or YUYV
    output. It do however probe all function blocks of the RPPX1 and provide
    an interface to interact with both parameter and statistic bufferers.
    The user of the framework will not change as algorithms for the
    different function blocks of the ISP are being added.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Jai Luthra <jai.luthra+renesas@ideasonboard.com>

---
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2025 Renesas Electronics Corp.
+ * Copyright 2025 Niklas Söderlund <niklas.soderlund@ragnatech.se>
+ */
+
+#include <media/v4l2-isp.h>
+#include <media/videobuf2-v4l2.h>
+
+#include "rppx1.h"
+
+#define RPPX1_PARAMS_BLOCK_INFO(block, data) \
+	[RPPX1_PARAMS_BLOCK_TYPE_ ## block] = { \
+		.size = sizeof(struct rppx1_params_ ## data ## _config), \
+	}
+
+static const struct v4l2_isp_params_block_type_info
+rppx1_ext_params_blocks_info[] = {
+};
+
+int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
+		 rppx1_reg_write write, void *priv)
+{
+	struct vb2_v4l2_buffer *vbuf = to_vb2_v4l2_buffer(vb);
+	struct v4l2_isp_params_buffer *cfg;
+	size_t block_offset;
+	int ret;
+
+	ret = v4l2_isp_params_validate_buffer_size(rpp->dev, vb, max_size);
+	if (ret)
+		return ret;
+
+	cfg = vb2_plane_vaddr(&vbuf->vb2_buf, 0);
+
+	ret = v4l2_isp_params_validate_buffer(rpp->dev, vb,
+					      (struct v4l2_isp_params_buffer *)cfg,
+					      rppx1_ext_params_blocks_info,
+					      ARRAY_SIZE(rppx1_ext_params_blocks_info));
+	if (ret)
+		return ret;
+
+	/* Walk the list of parameter blocks and process them. */
+	block_offset = 0;
+	while (block_offset < cfg->data_size) {
+		const union rppx1_params_block *block =
+			(const union rppx1_params_block *)&cfg->data[block_offset];
+		struct rpp_module *module;
+		int ret;
+
+		block_offset += block->header.size;
+
+		switch (block->header.type) {
+		default:
+			module = NULL;
+			break;
+		}
+
+		if (!module) {
+			pr_warn("Not handled RPPX1 block type: 0x%04x\n", block->header.type);
+			continue;
+		}
+
+		ret = rpp_module_call(module, fill_params, block, write, priv);
+		if (ret) {
+			pr_err("Error processing RPPX1 block type: 0x%04x\n", block->header.type);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(rppx1_params);
