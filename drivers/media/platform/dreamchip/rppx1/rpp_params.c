126c11f48b43545e5bb7a3689badf3b03729c8c1
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-03-14 22:59:37 +0100

    media: rppx1: Add support for Auto Exposure Measurement
    Extend the RPPX1 driver to allow setting the EXM configuration using the
    parameter buffer format. It uses the RPPX1 framework for parameters and
    its writer abstraction to allow the user to control how (and when)
    configuration is applied to the RPPX1.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Jai Luthra <jai.luthra+renesas@ideasonboard.com>

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -18,6 +18,7 @@ static const struct v4l2_isp_params_block_type_info
 rppx1_ext_params_blocks_info[] = {
 	RPPX1_PARAMS_BLOCK_INFO(AWB_GAIN, awb_gain),
 	RPPX1_PARAMS_BLOCK_INFO(AWB_MEAS, awb_meas),
+	RPPX1_PARAMS_BLOCK_INFO(AEC_MEAS, aec),
 };
 
 int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
@@ -57,6 +58,9 @@ int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
 			break;
 		case RPPX1_PARAMS_BLOCK_TYPE_AWB_MEAS:
 			module = &rpp->post.wbmeas;
+			break;
+		case RPPX1_PARAMS_BLOCK_TYPE_AEC_MEAS:
+			module = &rpp->pre1.exm;
 			break;
 		default:
 			module = NULL;
