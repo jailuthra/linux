08ffd756663e732e1f918119d3afb8b0d9a044bc
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-03-14 22:59:42 +0100

    media: rppx1: Add support for Gamma Correction
    Extend the RPPX1 driver to allow setting the Gamma Correction
    configuration using the parameter buffer format. It uses the RPPX1
    framework for parameters and its writer abstraction to allow the user to
    control how (and when) configuration is applied to the RPPX1.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Jai Luthra <jai.luthra+renesas@ideasonboard.com>

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -19,6 +19,7 @@ rppx1_ext_params_blocks_info[] = {
 	RPPX1_PARAMS_BLOCK_INFO(BLS, bls),
 	RPPX1_PARAMS_BLOCK_INFO(AWB_GAIN, awb_gain),
 	RPPX1_PARAMS_BLOCK_INFO(CTK, ctk),
+	RPPX1_PARAMS_BLOCK_INFO(GOC, goc),
 	RPPX1_PARAMS_BLOCK_INFO(LSC, lsc),
 	RPPX1_PARAMS_BLOCK_INFO(AWB_MEAS, awb_meas),
 	RPPX1_PARAMS_BLOCK_INFO(HST_MEAS, hst),
@@ -65,6 +66,9 @@ int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
 			break;
 		case RPPX1_PARAMS_BLOCK_TYPE_CTK:
 			module = &rpp->post.ccor;
+			break;
+		case RPPX1_PARAMS_BLOCK_TYPE_GOC:
+			module = &rpp->hv.ga;
 			break;
 		case RPPX1_PARAMS_BLOCK_TYPE_LSC:
 			module = &rpp->pre1.lsc;
--- drivers/media/platform/dreamchip/rppx1/rppx1_ga.c
+++ drivers/media/platform/dreamchip/rppx1/rppx1_ga.c
@@ -43,7 +43,41 @@ static int rppx1_ga_start(struct rpp_module *mod,
 	return 0;
 }
 
+static int
+rppx1_ga_fill_params(struct rpp_module *mod,
+		     const union rppx1_params_block *block,
+		     rppx1_reg_write write, void *priv)
+{
+	const struct rppx1_params_goc_config *cfg = &block->goc;
+
+	/* If the modules is disabled, simply bypass it. */
+	if (cfg->header.flags & V4L2_ISP_PARAMS_FL_BLOCK_DISABLE) {
+		write(priv, mod->base + GAMMA_OUT_ENABLE_REG, 0);
+		return 0;
+	}
+
+	write(priv, mod->base + GAMMA_OUT_MODE_REG,
+	      cfg->mode ? GAMMA_OUT_ENABLE_GAMMA_OUT_EN : 0);
+
+	/*
+	 * The native params are 24-bit while the RPP can be 12 or 24 bit.
+	 * Figure out how much we need to adjust the input values.
+	 */
+	const unsigned int shift = 24 - mod->info.ga.colorbits;
+
+	for (unsigned int i = 0; i < RPPX1_GAMMA_OUT_MAX_SAMPLES; i++)
+		write(priv, mod->base + GAMMA_OUT_Y_REG(i),
+		      cfg->gamma_y[i] >> shift);
+
+	/* Enable module. */
+	write(priv, mod->base + GAMMA_OUT_ENABLE_REG,
+	      GAMMA_OUT_ENABLE_GAMMA_OUT_EN);
+
+	return 0;
+}
+
 const struct rpp_module_ops rppx1_ga_ops = {
 	.probe = rppx1_ga_probe,
 	.start = rppx1_ga_start,
+	.fill_params = rppx1_ga_fill_params,
 };
